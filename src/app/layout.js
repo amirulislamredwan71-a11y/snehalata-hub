6c3df043ba9742c2c29eaea633cada498e633c96
K .M. A.I. REDWAN <amirulislamredwan71@gmail.com>
2026-02-05 19:09:38 +0600

    Change HTML lang attribute from 'en' to 'bn'

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,8 +7,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="bn">
       <head>
+        {/* Tailwind CSS সরাসরি লোড করা হচ্ছে যাতে ডিজাইন না ভাঙে */}
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet" />
       </head>
       <body>{children}</body>
