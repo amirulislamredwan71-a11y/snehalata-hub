b9b04aac754e719b90c59cda9f71ae311650e843
K .M. A.I. REDWAN <amirulislamredwan71@gmail.com>
2026-02-05 12:14:13 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
-module.exports = {
-  images: { domains: ['images.unsplash.com'] }
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
