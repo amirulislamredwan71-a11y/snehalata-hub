5c3f77b279b1675fd6447c7257fcd43a0d74f46d
K .M. A.I. REDWAN <amirulislamredwan71@gmail.com>
2026-02-07 09:02:52 +0600

    Update prisma.js

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,13 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
+let prisma
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
 }
 
-const globalForPrisma = globalThis
-
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
-
 export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
