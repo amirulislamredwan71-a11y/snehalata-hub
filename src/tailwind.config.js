2c9bafe85a11d26b753083ff36f6f8abc67ccce0
K .M. A.I. REDWAN <amirulislamredwan71@gmail.com>
2026-02-07 11:32:03 +0600

    Create tailwind.config.js

---
+++ src/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          500: '#9333ea',
+          600: '#7c3aed',
+          900: '#581c87',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
